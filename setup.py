bf651b6cc4440b4e2c4366186b523c6c08154e11
Kevin Coakley <kcoakley@sdsc.edu>
2016-10-04 18:10:25 -0700

    Added project skeleton

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+    extra = dict(install_requires=[
+    ],
+        include_package_data=True,
+        test_suite="tests.suite.load_tests",
+    )
+except ImportError:
+    from distutils.core import setup
+    extra = {}
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(name="ec2-cost-estimator",
+      version="0.0.1",
+      description="Estimate EC2 Costs for on-demand and spot instances",
+      long_description=readme(),
+      author="Kevin Coakley",
+      author_email="kcoakley@sdsc.edu",
+      scripts=[
+          "bin/ec2costestimator",
+      ],
+      url="",
+      packages=[
+          "ec2costestimator",
+          "ec2costestimator/cost",
+      ],
+      platforms="Posix; MacOS X",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+      ],
+      **extra
+      )
