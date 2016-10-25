54aed510ea90d8cfcfe4494749edcd07d084b8cc
Kevin Coakley <kcoakley@sdsc.edu>
2016-10-25 15:41:08 -0700

    Created function to estimate the cost of a spot instance

--- ec2costestimator/cost/spot.py
+++ ec2costestimator/cost/spot.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
 import boto3
+import datetime
+import pytz
+from ec2costestimator.ec2.instance_information import InstanceInformation
 
 
 class Spot:
 
     def __init__(self, aws_access_key_id, aws_secret_access_key, region):
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.region = region
 
         self.client = boto3.client('ec2',
-                                   aws_access_key_id=aws_access_key_id,
-                                   aws_secret_access_key=aws_secret_access_key,
-                                   region_name=region)
+                                   aws_access_key_id=self.aws_access_key_id,
+                                   aws_secret_access_key=self.aws_secret_access_key,
+                                   region_name=self.region)
 
     def get_current_cost(self, availability_zone, instance_type, product_descriptions="Linux/UNIX"):
         cost = self.describe_spot_price_history(availability_zone,
@@ -26,3 +32,51 @@ class Spot:
                                                        AvailabilityZone=availability_zone,
                                                        ProductDescriptions=[product_descriptions],
                                                        MaxResults=max_results)
+
+    def get_instance_cost(self, instance_id):
+
+        instance_information = InstanceInformation(self.aws_access_key_id,
+                                                   self.aws_secret_access_key,
+                                                   self.region)
+
+        launch_time = instance_information.get_instance_launch_time(instance_id)
+        hours = instance_information.get_instance_running_hours(instance_id)
+        instance_type = instance_information.get_instance_type(instance_id)
+        availability_zone = instance_information.get_availability_zone(instance_id)
+
+        # Calculate the number of spot price history results based on the number of hours the
+        # instance has been running
+        max_results = hours * 100
+
+        cost = self.describe_spot_price_history(availability_zone, instance_type,
+                                                max_results=max_results)
+
+        last_timestamp = pytz.utc.localize(datetime.datetime.utcnow())
+        total_cost = float(0)
+
+        for spot_price_history in cost["SpotPriceHistory"]:
+            # Calculate how long the SpotPrice is valid (the time_delta)
+            time_delta = last_timestamp - spot_price_history["Timestamp"]
+
+            # Calculate the hourly cost of the instance for that time_delta based on the SpotPrice
+            total_cost += float(float(spot_price_history["SpotPrice"]) *
+                                (time_delta.seconds / 3600))
+
+            last_timestamp = spot_price_history["Timestamp"]
+
+            # Stop looping through the SpotPriceHistory once the SpotPriceHistory Timestamp is
+            # older than the instance launch time
+            if launch_time > spot_price_history["Timestamp"]:
+                break
+
+        return round(total_cost, 4)
+
+    def get_instances_cost(self, instance_ids):
+
+        total = 0
+
+        if type(instance_ids) is list:
+            for instance_id in instance_ids:
+                total += float(self.get_instance_cost(instance_id))
+
+        return round(total, 4)
--- tests/test_spot.py
+++ tests/test_spot.py
@@ -3,6 +3,7 @@
 import fakes
 import unittest
 from mock import patch
+from mock import MagicMock
 import datetime
 from dateutil.tz import tzutc
 from ec2costestimator.cost.spot import Spot
@@ -35,3 +36,147 @@ class SpotTestCase(unittest.TestCase):
         spot_history = spot.get_current_cost("us-east-1b", "m3.medium")
 
         self.assertEqual(spot_history, "0.012200")
+
+    @patch('boto3.resource', fakes.FakeBoto3)
+    @patch.object(fakes.FakeBoto3, 'Instance')
+    @patch('boto3.client', fakes.FakeBoto3)
+    @patch.object(fakes.FakeBoto3, 'describe_spot_price_history')
+    def test_spot_get_instance_cost(self, mock_describe_spot_price_history, mock_instance):
+
+        instance = MagicMock()
+        instance.launch_time = datetime.datetime(2016, 1, 1, 1, 0, 0, tzinfo=tzutc())
+        instance.instance_type = "m3.medium"
+        instance.placement = {"AvailabilityZone": "us-east-1b"}
+
+        mock_instance.return_value = instance
+
+        mock_describe_spot_price_history.return_value = {
+            'NextToken': 'abc123',
+            'SpotPriceHistory': [
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 10, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.012200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 5, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.013200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 2, 30, tzinfo=tzutc()),
+                    'SpotPrice': '0.012200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 0, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.013200',
+                    'InstanceType': 'm3.medium'
+                }
+            ]
+        }
+
+        # Compute the spot price for the instances
+        spot_instance_cost = .0122 * (1200 / 3600)
+        spot_instance_cost += .0132 * (300 / 3600)
+        spot_instance_cost += .0122 * (150 / 3600)
+        spot_instance_cost += .0132 * (150 / 3600)
+        spot_instance_cost = round(spot_instance_cost, 4)
+
+        spot = Spot("ABC", "123", "us-east-1")
+
+        strptime = datetime.datetime.strptime
+
+        # Test a 30 minute running time
+        dt = datetime.datetime(2016, 1, 1, 1, 30, 0)
+        with patch('datetime.datetime') as datetime_mock:
+            datetime_mock.utcnow.return_value = dt
+            datetime_mock.strptime = strptime
+
+            output = spot.get_instance_cost("i-12345678")
+            self.assertEqual(output, spot_instance_cost)
+
+    @patch('boto3.resource', fakes.FakeBoto3)
+    @patch.object(fakes.FakeBoto3, 'Instance')
+    @patch('boto3.client', fakes.FakeBoto3)
+    @patch.object(fakes.FakeBoto3, 'describe_spot_price_history')
+    def test_spot_get_instances_cost(self, mock_describe_spot_price_history, mock_instance):
+
+        instance = MagicMock()
+        instance.launch_time = datetime.datetime(2016, 1, 1, 1, 0, 0, tzinfo=tzutc())
+        instance.instance_type = "m3.medium"
+        instance.placement = {"AvailabilityZone": "us-east-1b"}
+
+        mock_instance.return_value = instance
+
+        mock_describe_spot_price_history.return_value = {
+            'NextToken': 'abc123',
+            'SpotPriceHistory': [
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 10, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.012200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 5, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.013200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 2, 30, tzinfo=tzutc()),
+                    'SpotPrice': '0.012200',
+                    'InstanceType': 'm3.medium'
+                },
+                {
+                    'AvailabilityZone': 'us-east-1b',
+                    'ProductDescription': 'Linux/UNIX',
+                    'Timestamp': datetime.datetime(2016, 1, 1, 1, 0, 0, tzinfo=tzutc()),
+                    'SpotPrice': '0.013200',
+                    'InstanceType': 'm3.medium'
+                }
+            ]
+        }
+
+        # Compute the spot price for the instances
+        spot_instance_cost = .0122 * (1200 / 3600)
+        spot_instance_cost += .0132 * (300 / 3600)
+        spot_instance_cost += .0122 * (150 / 3600)
+        spot_instance_cost += .0132 * (150 / 3600)
+        spot_instance_cost = round(spot_instance_cost, 4)
+
+        spot = Spot("ABC", "123", "us-east-1")
+
+        strptime = datetime.datetime.strptime
+
+        # Test 1 instance with 30 minute running time
+        dt = datetime.datetime(2016, 1, 1, 1, 30, 0)
+        with patch('datetime.datetime') as datetime_mock:
+            datetime_mock.utcnow.return_value = dt
+            datetime_mock.strptime = strptime
+
+            output = spot.get_instances_cost(["i-12345678"])
+            self.assertEqual(output, spot_instance_cost)
+
+        # Test 5 instance with 30 minute running time
+        dt = datetime.datetime(2016, 1, 1, 1, 30, 0)
+        with patch('datetime.datetime') as datetime_mock:
+            datetime_mock.utcnow.return_value = dt
+            datetime_mock.strptime = strptime
+
+            output = spot.get_instances_cost(["i-12345678", "i-23456789", "i-345678901",
+                                              "i-456789012", "i-567890123"])
+            self.assertEqual(output, spot_instance_cost * 5)
